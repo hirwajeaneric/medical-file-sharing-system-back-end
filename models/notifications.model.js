466c43327162fe939666fa667fe283db4b9238d1
hirwajeaneric <hirwaminerve25@gmail.com>
2023-07-23 09:56:40 +0200

    Removed uploads from being uploaded

--- models/notifications.model.js
+++ models/notifications.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const notificationsModel = new mongoose.Schema({
     time: {type: Date, required: true, default: Date.now()},
-    severity: {type: String, required: true},
-    message: {type: String, required: true},
-    viewed: {type: Boolean, required: true, default: false},
+    severity: {type: String, required: false},
+    message: {type: String, required: false},
+    viewed: {type: Boolean, required: false, default: false},
 });
 
 const Notifications = mongoose.model('notifications', notificationsModel);
